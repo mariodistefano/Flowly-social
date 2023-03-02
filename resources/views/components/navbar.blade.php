ae59eaa5a136279d6071a75ccdb33ea415f87ab7
mario-di-stefano <mariodistefanoo@live.it>
2023-03-02 18:30:13 +0100

    aggiunta la navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,43 +1,76 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="{{route('homepage')}}">Home</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+ <nav id="navbar" class="navbar-expand-lgMIA d-none d-md-block">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          
+          </button>
+        <div class="container h-100">
+            <div class="row h-100 align-items-around" class="collapse navbar-collapse" id="navbarSupportedContent"">
 
-        @guest
+                <div class="col-12">
 
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Accedi
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="{{route('register')}}">Registrati</a></li>
-            <li><a class="dropdown-item" href="{{route('login')}}">Login</a></li>
-          </ul>
-        </li>
-        
-        @else
-        
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Benvenuto {{Auth::user()->name}}
-          </a>
-          <ul class="dropdown-menu">
-        </li>
-          <li><a class="dropdown-item" href="#" onclick="event.preventDefault(); document.querySelector('#form-logout').submit();">Logout</a>
-          </li>
-      </ul>
-        <form method="POST" id="form-logout" action="{{route('logout')}}" class="d-none"> @csrf</form>
+                    <ul class="h-100 navbar-nav flex-column justify-content-around">
+                        <li >
+                            <h2>Aulab Post</h2>
+                        </li>
+                        <li> 
+                            <button class="  ms-2 button " >
+                                clicca
+                            </button>
+                        </li>
+                        <li> 
+                            <button class="  ms-2 button"  >
+                                clicca
+                            </button>
+                        </li>
+                        <li> 
+                            <button class="  ms-2 button"  >
+                                clicca
+                            </button>
+                        </li>
+                        <li> 
+                            <button class="mb-5  ms-2 button"  >
+                                clicca
+                            </button>
+                        </li>
+                        <li>
+                            <button class="m-5 text-start w-100  ms-2 button"  >
+                                Logout
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="gost"></div>
 
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="{{route('article.create')}}">Crea Articolo</a>
-        </li>
-
-        @endguest
-
+   <div class="offcanvas offcanvas-start show" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasLabel">
+        <div class="offcanvas-header">
+              <h2 class="offcanvas-title" id="offcanvasLabel">Aulab Post</h2>
+              <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body flex-column justify-content-center h-100">
+                <button class="col-12 button mt-3">
+                    clicca
+                </button>
+                <button class="col-12 button mt-3">
+                    clicca
+                </button>
+                <button class="col-12 button mt-3">
+                    clicca
+                </button>
+                <div>
+                <button class="col-12 button mt-5">
+                    Logout
+                </button>
+            </div>
+        </div>
+         
     </div>
-  </div>
-</nav>
+    <a class="m-5 d-md-none" data-bs-toggle="offcanvas" href="#offcanvasExample"  aria-controls="offcanvasExample">
+        <span class="navbar-toggler-icon">   <label for="burger" class="burger mt-5">
+            <input id="burger" type="checkbox">
+            <span></span>
+            <span></span>
+            <span></span>
+          </label></span>
+      </a>
