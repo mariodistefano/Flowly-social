6a1eab4fa0852553fa709389b460f091a00e20c7
mario-di-stefano <mariodistefanoo@live.it>
2023-03-02 23:38:29 +0100

    aggiunto padding per display medi al form di registrazione

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,10 +11,10 @@
     <form method="POST" action="{{route('register')}}" id="form" class="col-12 formRegLog col-md-5 px-4">
         @csrf
       <div class="flex-column align-items-center justify-content-center">
-        <h3 class="mt-4 text-center">Sing up</h3>
+        <h3 class="mt-4 text-center pt-5 pt-md-2">Sing up</h3>
         <p class="text-center text-secondary">create a free account with your email</p>
       </div>
-      <div class="px-md-5 p-0">
+      <div class="px-md-5 p-0 pt-3">
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label ps-3 font-bold">Name</label>
         <input type="text" class="form-control radius" id="exampleInputEmail1" aria-describedby="emailHelp" name="name">
