fbab07af83b9a28a0668abcd3456b274159e215a
mario-di-stefano <mariodistefanoo@live.it>
2023-03-03 17:33:11 +0100

    problemino rotta del nome articolo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,22 +13,64 @@
         <div class="row justify-content-center">
 
             @foreach ($articles as $article)
-                <div class="col-12 col-md-6-col-lg-6">
-                    <div class="card" style="width: 18rem;">
-                        <img src="{{Storage::url($article->img)}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <h5 class="card-title">{{$article->title}}</h5>
-                          <p class="card-text">{{$article->subtitle}}</p>
-                          <p class="card-text">{{$article->category->name}}</p>
-                          <p class="card-text">{{$article->body}}</p>
-                          <p class="card-text"><small class="text-muted">Data: {{$article->created_at->format('d/m/y')}}</small></p>
-                          {{-- <a href="{{route('article.detail', ['title' => $article->title])}}" class="btn btn-success">Dettaglio</a> --}}
-                          <a href="{{route('article.show', compact('article'))}}" class="btn btn-primary">Dettaglio</a>
-                          <a href="{{route('article.byCategory', ['category' => $article->category->id])}}" class="btn btn-primary small fst-italic text-capitalize">{{$article->category->name}}</a>
-                          <a href="{{route('article.byUser', ['user' => $article->user->id])}}" class="btn btn-primary small fst-italic text-capitalize">{{$article->user->name}}</a>
-                        </div>
-                    </div>
-                </div>
+               <!-- card -->
+ <div class="container-fluid px-5 mt-5 d-none d-md-block margine-prova">
+    <div class="row  justify-content-between  primacard ">
+      <div class="col-12 flex-column articleCellular justify-content-center aling-items-center col-md-3">
+        <div class="col-3 cerchio my-4 ms-2 my-md-5 ms-md-3">
+        </div>
+        <!-- <div class="articleCellular"> -->
+        <span class="col-3"><h5 class="text-center pe-md-4 text-bold"> <a href="{{route('article.byUser')}}">{{$article->user->name}}</a></h5></span>
+        <span class="col-3"><p class="text-center text-secondary pe-md-4">categoria : {{$article->category->name}}</p></span>
+        <span class="col-3"><p class="text-center pe-md-4">data inserimento: {{$article->created_at->format('d/m/y')}}</p></span>
+      </div>
+    <!-- </div> -->
+        <div class="col-12 col-md-5 pt-5 mt-md-3 me-md-4">
+          <h1> <strong>{{$article->title}}</strong></h1>
+          <h6 class="text-secondary">{{$article->subtitle}}</h6>
+          <p class="font-sizeS">{{$article->body}}</p>
+         <button class="btnArticle"><a href="{{route('article.show', compact('article'))}}" class="btn btn-primary">Dettaglio</a></button>
+        </div>
+          <div class="col-12 col-md-3 h-100 py-md-4 me-md-3 p-0">
+            <img class="ImgCard p-3 me-2" src="{{Storage::url($article->img)}}" alt="immagine">
+          </div>
+       </div> 
+    </div>
+    
+    
+    <!-- article cellulare -->
+    
+    <div class="container-fluid  mt-5 pb-5 d-block d-md-none">
+    <div class="row  justify-content-between  primacard pb-4">
+     <div class="row  articleCellular justify-content-center aling-items-center ">
+       <div class="col-3 cerchio my-4 ms-2 my-md-5 ms-md-3">
+       </div>
+       <div class="col-8 mt-5 flex-column">
+       <span class="col-3"><h5 class="text-center text-bold"><a href="{{route('article.byUser')}}">{{$article->user->name}}</a></h5></span>
+       <span class="col-3"><p class="text-center text-secondary pe-md-4">categoria : {{$article->category->name}}</p></span>
+       <span class="col-3"><p class="text-center pe-md-4">data inserimento:{{$article->created_at->format('d/m/y')}}</p></span>
+     
+    </div>
+    </div>
+       <div class="col-12 col-md-5  mt-md-3 me-md-4">
+         <div class="col-12 col-md-3 h-100 py-md-4 me-md-3 p-0">
+           <img class="ImgCard p-1 me-2" src="{{Storage::url($article->img)}}" alt="immagine">
+         
+         <h1> <strong>{{$article->title}}</strong></h1>
+         
+         <h6 class="text-secondary">{{$article->subtitle}}</h6>
+         <p class="font-sizeS">{{$article->body}}</p>
+        
+         <div class="d-flex justify-content-end"><button class="btnArticle me-2 "><a href="{{route('article.show', compact('article'))}}" class="btn btn-primary">Dettaglio</a></button></div>
+       </div>
+        
+        
+         <!--qui  -->
+        </div>
+      </div> 
+      
+    </div>
+       
             @endforeach
 
         </div>
