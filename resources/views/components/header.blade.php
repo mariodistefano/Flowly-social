4bdfb2c40d9dae8bbbbd3831f02d7829fce69375
mario-di-stefano <mariodistefanoo@live.it>
2023-03-03 19:42:59 +0100

    aggiunto header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,9 +1,16 @@
-<div class="container-fluid my-3">
-    <div class="row justify-content-center text-center">
-        <div class="col-12">
-            <h1>
-                {{$title}}
-            </h1>
-        </div>
-    </div>
+
+<header class="container-fluid ">
+    <div class="row h-100 align-items-center">
+      <h3 class="text-center mt-2 d-md-none d-block  border-bottomMIO">Aulab.post</h3>
+      <div class="col-md-8 ">
+          <h4 class="ps-4 benvenuto d-none d-md-block pt-5"> {{$title}}</h4>
+          <h6 class="text-secondary ps-4 d-none d-md-block">sfoglia mille e oltre articoli fantastici</h6>
+      </div>
+<!-- <div class="col-2 d-none d-md-block d-flex">    -->
+    <button class="btnHEADER d-none d-md-block  mb-md-5 me-md-3">Vai Dettaglio</button>
+    <button class="btnHEADER d-none d-md-block  mb-md-5 me-md-3">Vai Dettaglio</button>
+    <button class="btnLINGUA d-none d-md-block  mb-md-5 me-md-3">IT</button>
+<!-- </div> -->
 </div>
+  
+</header> 
