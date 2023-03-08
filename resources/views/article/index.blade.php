45b405da2adfd99e00821be8393f26c26f16f883
Corrado Floriddia <corr2flo@gmail.com>
2023-03-08 17:20:26 +0100

    nav2

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -3,14 +3,12 @@
         title="Tutti gli Articoli"
     />
     <div class="scrolling-wrapper px-3 pt-1">
-        @foreach ($articles as $article)
+        @foreach ($categories as $category)
            <div class="card position-relative">
-                    <img class="card " src="{{Storage::url($article->img)}}" alt="immagine">
+                    {{-- <img class="card " src="{{Storage::url($article->img)}}" alt="immagine"> --}}
+                    {{-- <img class="card " src="{{Storage::url($article->img)}}" alt="immagine"> --}}
                 <div class="posizioneCard background z-index h-100 w-100">
-                    <h5 class="pt-5 mt-5 ps-1"> <a class="font-card1 text-white font-bold " href="">{{substr($article->title, 0, 16)}}</a> </h5>
-                    <p class="font-card ps-1  text-white z-index">categoria : {{$article->category->name}}
-                    </p> 
-                    <p class=" font-card ps-1 text-white z-index">data inserimento: {{$article->created_at->format('d/m/y')}}</p>
+                    <h5 class="pt-5 mt-5 ps-1"> <a class="font-card1 text-white font-bold " href="{{route('article.byCategory', ['category' => $article->category->id])}}">{{$article->category->name}}</h5>
                 </div>
            </div> 
         @endforeach
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,23 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Aulab Post</title>
+    {{-- linknavbar --}}
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+    
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
-<body>
-    <div class="container">
+<body class="">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-2">
+
+            <div class="col-3 mx-0 px-0 vh-100">
                 <x-navbar />
 
             </div>
-            <div class="col-10">
+
+            <div class="col-9 mx-0 p-0">
                 <div class="">
 
                     {{$slot}}
                     {{-- <x-footer /> --}}
                 </div>
-
             </div>
+
         </div>
     </div>
     
